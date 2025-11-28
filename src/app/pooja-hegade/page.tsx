8d846f638e7eccc564152694f46d4f981fdb49e9
HritikGangadhar <hritikdev24@gmail.com>
2025-11-28 13:52:41 +0530

    share option added

--- src/app/pooja-hegade/page.tsx
+++ src/app/pooja-hegade/page.tsx
@@ -12,7 +12,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url:`${images[3].src}`,
+        url:images[3].src,
         width: 1200,
         height: 630,
       },
@@ -22,7 +22,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Pooja Hegade Gallery",
     description: "Explore the beautiful Pooja Hegade photo and video gallery.",
-    images: [`${images[3].src}`] // FIXED syntax
+    images: [images[3].src] // FIXED syntax
   },
 };
 
